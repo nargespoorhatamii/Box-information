e488af49d2c2f31fddf33538200499f4d61e46bd
nargespoorhatamii <126357653+nargespoorhatamii@users.noreply.github.com>
2023-04-11 21:56:18 +0430

    Add project files.

---
+++ Box information/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace project
+{
+    class test
+    {
+        public static  void Main()
+        {
+            Console.WriteLine("inter lenth");
+            int L = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("inter width");
+            int W = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("inter hieght");
+            int H = Convert.ToInt32(Console.ReadLine());
+            calc(L, W, H);
+        }
+        private static void calc(int x, int y, int z)
+        {
+            Double Result = x * y * z;
+            Console.WriteLine(Result);
+        }
+    }
+}
